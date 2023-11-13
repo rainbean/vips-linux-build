ca3dafa9b26b09ca30631a0dbeafec5b60778352
Jimmy Lee <Jimmy.m.Lee@gmail.com>
2023-11-13 17:04:39 +0800

    update openslide tag

--- build/openslide.sh
+++ build/openslide.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Build openslide"
-OPENSLIDE_VERSION=2023-09-07
+OPENSLIDE_VERSION=2023-11-13
 wget -q https://github.com/rainbean/openslide/releases/download/${OPENSLIDE_VERSION}/openslide-3.4.1.tar.gz
 tar xf openslide-3.4.1.tar.gz
 cd openslide-3.4.1
